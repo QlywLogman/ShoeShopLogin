0b08e8ec5f587dd393880fe8c1c3fabb14149934
quliy_nx74 <quliy_nx74@STHQ0118-06>
2024-08-30 14:20:41 +0400

    Add project files.

---
+++ ShoeShopLogin/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ShoeShopLogin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
